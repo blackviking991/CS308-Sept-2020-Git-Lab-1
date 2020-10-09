4db12bd8618ee2d96136dbdd8f83a74569a988bb
blackviking991 <ishaanrock138@gmail.com>
2020-10-09 20:47:47 +0530

    Added changes to new branch: br_inputCheck

--- sqrt.c
+++ sqrt.c
@@ -4,18 +4,26 @@ positive number. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-// ENter the number you want the sqrt for 
+// ENter the number you want the sqrt for
 
-int main(int argc, char* argv[]) {
-	
-	if (argc != 2) {
+int main(int argc, char *argv[])
+{
+
+	if (argc != 2)
+	{
 		printf("Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
-	printf("Sqrt of %d is %f\n",input, sqrt(input));
+	if (input < 0)
+	{
+		printf("Not a valid number \n");
+		exit(-1);
+	}
+
+	printf("Sqrt of %d is %f\n", input, sqrt(input));
 	printf("End of the program, Exiting\n");
-	return(0);
+	return (0);
 
 } // end main
